4626b9b88bbc61f5a31add09ac7610a1edc32d5b
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-02 22:06:01 +0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,10 +1,12 @@
 public class main {
   public static void main(String[] args) {
-    //memanggil hello word
+    // memanggil hello word
     /*memanggil
     * hello
     * word
     */
+
+    /** oke word */
         System.out.println("Hello, World!");
     }
     
