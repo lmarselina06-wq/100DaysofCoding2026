0b225c1e7df019eb0e305f67c449a92479ee999f
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-04 09:53:11 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,25 @@
+public class Day3{
+    public static void main(String[] args) {
+
+        //1. \n = enter(ketika di run iya membuat 3 baris terpisah)
+        System.out.println("Baris 1\nBaris 2\nBaris 3\n");
+
+        //2.  \t = tab/membuat teks rata kanan agar terlihat rapi dan bisa membuat baris baru tanpa menggunakan println
+        //3. manfaat \t yaitu membuat kolom agar kelihatan seperti tabel
+        System.out.println("Nama:\tMarselina");
+        System.out.println("Umur:\t20");
+
+        //4. gabungan \n dan \t = membuat tabel  sederhana
+        System.out.println("No\tNama\tUmur");
+        System.out.println("1\tMarselina\t18");
+        System.out.println("3\tDelsa\t18");
+
+        //5. \" = kutip didalam kutip(menampilkan tanda kutip)
+        System.out.println("dua kalimat untuk saya: \"Semangat ngodingnya");
+
+        //6. \\ = path file(alamat file agar komputer tau file berada dimana)
+        System.out.println("Lokasi file: C:\\user\\data");
+        
+    }
+
+}
