6fbe7a1c489c1223975ce86576154c898abe80d2
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-05 19:37:09 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -6,16 +6,16 @@ public class Day4{
     // short / ukuran = 2 byte / jumlah bit 16 bit
     // 2x lipat dari byte,buat data sedang
 
-    // int / ukuran = 3 byte / jumlah bit = 32 bit
+    // int / ukuran = 4 byte / jumlah bit = 32 bit
     // default bilangan bulat di java, paling sering di pakai
 
-    // long / ukuran =  4 bit / jumlah bit = 62 bit
+    // long / ukuran =  8 bit / jumlah bit = 62 bit
     // paling besar,buat angka besar
 
    byte angka1 = 20;
    short angka2 = 200;
    int angka3 = 1200;
-   long angka4 = 1400000;
+   long angka4 = 1400000L;
    System.out.println("nilai angka 1 = "+ angka1);
    System.out.println("nilai angka 2 = "+ angka2);
    System.out.println("nilai angka 3 = "+ angka3);
