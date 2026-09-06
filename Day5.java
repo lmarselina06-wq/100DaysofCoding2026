268a52f8a46e1ecc52b334188bd3904fcee18170
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-06 11:18:54 +0800

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,9 +1,9 @@
 public class Day5 {
     public static void main(String[] args) {
-        // ukuran float = 4 byte dan jumlah bit = 32 bit
+        // float = jumlah bit = 32 bit
         // float wajib menggunakan huruf f di belakang angka
         // jika tidak di gunakan maka akan error
-        // ukuran double = 8 byte dan jumlah bit = 64 bit
+        // double = jumlah bit = 64 bit
       
         float nilai = 89.5f;      
         double uang = 25000.50;   
