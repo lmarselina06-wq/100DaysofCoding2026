9ecece3d47228f0f9652cd83777d818b08962ba8
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-07 14:05:06 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,25 @@
+public class day6 {
+    public static void main(String[] args) {
+    /* marselina adalah mahasiswa baru. ia mau membuat 
+    program sederhana untukmendata dirinya sendiri
+    dari data berikut:
+    */
+    // 1. nama = marselina
+    // 2. umur marselina = 18 tahun
+    // 3. IPK marselina = 3,14
+    // 4. kode jurusan marselina = 'I' untuk informatika
+    // 5. status marselina = kuliah
+
+    String nama = "marselina";
+    int umur = 18;
+    double IPK = 3.14;
+    char kodeJurusan = 'I';
+    boolean status = true;
+    System.out.println("nama : " + nama);
+    System.out.println("umur : " + umur);
+    System.out.println("IPK : " + IPK);
+    System.out.println("kodeJurusan : " + kodeJurusan);
+    System.out.println("status : " + status);
+    }
+
+}
