43d98e454749e2f11da014b4bd7192d9b39753ec
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-08 16:14:24 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,11 @@
+public class day7{
+ public static void main(String[] args) {
+    // string tipe data yang digunakan untuk
+    // menyimpan kumpulan karakter karakter berupa teks
+    
+    String nama = "marselina";
+    System.out.println("nama : "+ nama);
+
+ }
+
+}
