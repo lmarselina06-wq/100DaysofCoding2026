6d0d610ffc8194642f9c5fdb0bff33f1e165d105
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-09 12:28:00 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,21 @@
+public class day8 {
+    public static void main(String[] args) {
+        // deklarasi 
+        String nama_player;
+        int level;
+        // inisialisasi
+        nama_player = "Marselina";
+        level = 2;
+        //tampilkan data awal
+        System.out.println("nama : " + nama_player);
+        System.out.println("level : " + level);
+
+        //update nilai variabel
+        nama_player = "Marsel";
+        level = 4;
+        //tampilkan nilai akhir
+        System.out.println("nama : " + nama_player);
+        System.out.println("level : " + level);
+    }
+    
+}
