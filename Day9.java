34662c35218325c7c15852c850c73224f44dea55
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-10 15:01:32 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+    public static void main(String[] args) {
+        final String nama = "Marselina";
+        final int Jumlah_bulan = 12;
+        final boolean mahasiswa = true;
+        System.out.println("nama : " + nama);
+        System.out.println("Jumlah_bulan : " + Jumlah_bulan);
+        System.out.println("mahasiswa : " + mahasiswa);
+
+    }
+
+}
