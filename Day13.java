7cdba26f2b93fd01b3e94179ea4f2a92bf689909
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-14 18:30:32 +0800

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class evaluasi1 {
+    public static void main(String[] args) {
+    Scanner in = new Scanner (System.in);
+    double nilai1,nilai2;
+    System.out.print("masukkan nilai1");
+    nilai1 = in.nextDouble();
+    System.out.print("masukkan nilai2");
+    nilai2 = in.nextDouble();
+    System.out.printf("nilai1: %.2f\n", nilai1);
+    System.out.printf("nilai2: %.2f\n", nilai2);
+    
+    }
+}
