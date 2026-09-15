ce2acdc62245ff07e62b184c381edf77418a2d18
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-15 17:59:08 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+public class day14 {
+    public static void main(String[] args) {
+    int angka1 = 25;
+    int angka2 = 15;
+
+    int hasi_perjumlahan = angka1 + angka2;
+    int hasil_pengurangan = angka1 - angka2;
+    
+    System.out.println("angka1: "+ angka1);
+    System.out.println("angka2: "+ angka2);
+    System.out.println("hasil penjumlahan: "+ hasi_perjumlahan);
+    System.out.println("hasil pengurangan: "+ hasil_pengurangan);
+
+    }
+}
