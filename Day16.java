205e79c7161a54bde62f59a890817bb91a29dedd
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-17 20:20:08 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,8 @@
+public class day16{
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 3;
+    int hasil = a % b;
+    System.out.println("hasil dari 10 % 3 = "+ hasil);
+    }
+}
