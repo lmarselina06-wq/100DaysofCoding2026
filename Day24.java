1e7620eaf445bf7b38c65dda9758ed3f131f5564
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-25 02:35:52 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,15 @@
+package koding.src;
+public class dayy {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+      
+        int luas = panjang * lebar;
+        System.out.println("=== Menghitung luas persegi panjang");
+        System.out.println("panjang = " + panjang);
+        System.out.println("lebar = " + lebar);
+        System.out.println("Luasnya adalah = " + luas);
+
+    }
+
+}
