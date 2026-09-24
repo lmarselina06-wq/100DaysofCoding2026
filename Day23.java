bbc90ab20f5f6f0516827a98ed73e301a0a12dac
lmarselina06-wq <lmarselina06@gmail.com>
2026-09-24 20:12:18 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+package koding.src;
+public class dayy {
+    public static void main(String[] args) {
+        int sisi = 10;
+        int luas = sisi * sisi;
+        System.out.println("sisi persegi = " + sisi);
+        System.out.println("luas persegi = " + luas);
+
+    }
+
+}
